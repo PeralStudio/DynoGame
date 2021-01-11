54d6a2caec239b1bcdc7edcec5249d78cc013319
Alberto Peral <info@peralstudio.com>
2021-01-11 16:22:25 +0100

    1

--- juego.js
+++ juego.js
@@ -33,10 +33,10 @@ function cargaImagenes(){
     imgCactus = new Image();
     imgSuelo = new Image();
 
-    imgRex.src = 'img/rex.png'
-    imgNube.src = 'img/nube.png'
-    imgCactus.src = 'img/cactus.png'
-    imgSuelo.src = 'img/suelo.png'
+    imgRex.src = '/img/Rex.png'
+    imgNube.src = '/img/Nube.png'
+    imgCactus.src = '/img/Cactus.png'
+    imgSuelo.src = '/img/Suelo.png'
 }
 
 
@@ -67,10 +67,16 @@ var nube = {x: 730, y: 20, velocidad: 1};
 var suelog = {x: 0, y: 242};
 
 function dibujaRex(){
+    cargaImagenes();
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
     ctx.drawImage(imgRex,0,0,69,69,100,trex.y,50,50)
 }
 
 function dibujarCactus(){
+    cargaImagenes();
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
     ctx.drawImage(imgCactus,0,0,60,60,cactus.x,cactus.y,60,60)
 }
 
@@ -85,6 +91,9 @@ function logicaCactus(){
 }
 
 function dibujarNube(){
+    cargaImagenes();
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
     ctx.drawImage(imgNube,0,0,75,75,nube.x,nube.y,75,75)
 }
 
@@ -99,6 +108,9 @@ function logicaNube(){
 
 
 function dibujarSuelo(){
+    cargaImagenes();
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
     ctx.drawImage(imgSuelo,suelog.x,0,2377,12,0,suelog.y,2377,12)
 }
 
@@ -160,13 +172,6 @@ function colision(){
     
 }
 
-function sonidoMuerto() {
-    for (let a = 0; a < 5; a++) {
-        // Se ejecuta 5 veces, con valores del paso 0 al 4.
-        console.log('Camina un paso hacia el este');
-      }
-}
-
 
 function puntuacion(){
     
@@ -194,13 +199,13 @@ setInterval(function(){
 function principal(){
     borraCanvas();
     gravedad();
+    dibujarCactus();
+    dibujaRex();
     colision();
     logicaCactus();
-    dibujarCactus();
     logicaNube();
     dibujarNube();
     logicaSuelo();
     dibujarSuelo();
-    dibujaRex();
     puntuacion();
 }
